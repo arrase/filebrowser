13da93a6da3cb3c0dbeb11b116d33622e4634708
Juan Ezquerro LLanes <arrase@gamil.com>
2020-02-15 19:57:30 +0100

    build

--- build-rpi0w.sh
+++ build-rpi0w.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+go get github.com/GeertJohan/go.rice
+go get github.com/GeertJohan/go.rice/rice
+
 cd frontend
 npm install
 npm run build
+
 cd ../http
 rice embed-go
+
 cd ..
+go mod download
 GOARM=6 GOARCH=arm GOOS=linux go build -ldflags="-s -w" -v -o filebrowser-arm
